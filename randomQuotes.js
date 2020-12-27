c9bb4e7a994b538ff1237856853fd9c69354ca6c
Gnv Saikiran <gajjarapusai1@gmail.com>
2020-12-27 09:58:18 +0530

    replace XMLHttpRequest with fetch API

--- randomQuotes.js
+++ randomQuotes.js
@@ -1,44 +1,29 @@
-const randomQuote = (callback) => {
-    const request = new XMLHttpRequest();
+const getQuote = async () => {
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+    return data;
+}
 
-    request.addEventListener('readystatechange', () => {
-        if (request.readyState === 4 && request.status === 200) {
-            const data = JSON.parse(request.responseText);
-            callback(undefined, data);
-        } else if (request.readyState === 4) {
-            callback('could not fetch the data', undefined);
-        }
-    });
+getQuote().then(data => {
+    let container = document.getElementById('container-div');
+    let quote = document.createElement('h3');
+    let author = document.createElement('p');
+    let tags = document.createElement('p');
 
-    request.open('GET', 'https://api.quotable.io/random');
-    request.send();
-};
+    quote.textContent = data.content;
+    author.textContent = `-${data.author}`;
 
-randomQuote((err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        let container = document.getElementById('container-div');
-        let quote = document.createElement('h3');
-        let author = document.createElement('p');
-        let tags = document.createElement('p');
+    container.appendChild(quote);
+    container.appendChild(author);
 
-        quote.textContent = data.content;
-        author.textContent = `-${data.author}`;
-
-        container.appendChild(quote);
-        container.appendChild(author);
-
-        console.log(`${data.content}\n-${data.author}`);
-        for (let tag of data.tags) {
-            tags.textContent += ` #${tag}`
-            console.log(`#${tag}`)
-        }
-        tags.setAttribute('id', 'tags')
-        container.appendChild(tags)
-
+    console.log(`${data.content}\n-${data.author}`);
+    for (let tag of data.tags) {
+        tags.textContent += ` #${tag}`
+        console.log(`#${tag}`)
     }
-});
+    tags.setAttribute('id', 'tags')
+    container.appendChild(tags)
+}).catch(err => console.log('could not fetch the data'));
 
 
 
